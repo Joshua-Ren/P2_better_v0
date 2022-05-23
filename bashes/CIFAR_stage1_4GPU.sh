92127187d75d569e7b1ec87380becb2db94c806a
Joshua-Ren <renyi.joshua@gmail.com>
2022-05-22 19:52:21 -0700

    inital

--- bashes/CIFAR_stage1_4GPU.sh
+++ bashes/CIFAR_stage1_4GPU.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=stage1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
+#SBATCH --cpus-per-task=2
 #SBATCH --time=36:00:00
 #SBATCH --mem-per-cpu=10G
 #SBATCH --output=./logs/stage1.txt 
