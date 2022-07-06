674cdd212bf9010fcde46153e28bca52f3e12a3b
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-06 00:22:00 -0700

    initial

--- bashes/LP_C10_STL10.sh
+++ bashes/LP_C10_STL10.sh
@@ -24,7 +24,7 @@ source /home/sg955/functorch-env/bin/activate
 cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_LP.py \
---batch_size 128  --dataset stl10 --figsize 224 --loss_type ce \
+--batch_size 128  --dataset cifar10 --figsize 224 --loss_type ce \
 --lr 0.001 \
 --work_dir ./results/C10_res18_PT \
 --alice_name Alice_resnet18_PT.pth \
