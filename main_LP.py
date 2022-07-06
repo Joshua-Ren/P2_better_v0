016a185734dd8b909bad0d8fb50a5fcf5404ce7e
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-05 23:23:13 -0700

    initial

--- main_LP.py
+++ main_LP.py
@@ -154,7 +154,8 @@ def main(args):
     # ================== Create the model and copy alice parameters ==================
     seed_model = get_init_net(args)
     alice_path = os.path.join(args.work_dir, args.alice_name)
-    mis_k, unex_k = load_checkpoint(args, seed_model, alice_path, which_part='alice')
+    mis_k, unex_k = load_checkpoint(args, seed_model, alice_path, which_part='all')
+    #mis_k, unex_k = load_checkpoint(args, seed_model, alice_path, which_part='alice')
     print('=======mis_k============')
     print(mis_k)
     print('=======unex_k============')
