d2e8eadc940be18e7d0af73ad3ecd61c4293e44b
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-06 18:58:16 -0700

    stupid bug

--- engine_LPFT.py
+++ engine_LPFT.py
@@ -97,8 +97,11 @@ def evaluate(data_loader, model, device, args, model0=None, train_type='ft'):
                 ztz0_cos.update(cos_dist,targets.size(0))
                 ztz0_norm.update(norm_dist,targets.size(0))
                 ztz0_dot.update(dot_dist.cpu(),targets.size(0))
-                print(zt_dist.shape)
-                print(ztz0_dot.avg)
+                print(dot_dist)
+                if torch.isfinite(dot_dist):
+                    print(zt)
+                    print(z0)
+                    xx=xx
                 zt_norm.update(zt_dist.cpu(),targets.size(0))
 
             hid = hid.detach()
