2d9b53c030e492990e1bc461c5dfbc7f03505374
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-10 09:49:44 -0700

    inital

--- main_FT.py
+++ main_FT.py
@@ -210,6 +210,8 @@ def main(args):
             if args.dataset[:6]=='domain':
                 vacc_o1 = evaluate_ood(data_loader_val_ood1, modelt, args.device, args,wb_title='ft_valid_ood1')
                 vacc_o2 = evaluate_ood(data_loader_val_ood2, modelt, args.device, args,wb_title='ft_valid_ood2')
+                results['vacc_o1'].append(vacc_o1)
+                results['vacc_o2'].append(vacc_o2)            
             tloss, tacc, grad_bob = train_one_epoch(modelt, criterion, data_loader_train, optimizer, scheduler, epoch, mixup_fn, args=args, train_type='ft')  
             if vacc >= best_vacc:
                 best_vacc = vacc
@@ -217,8 +219,6 @@ def main(args):
             results['tacc'].append(tacc)
             results['vloss'].append(vloss)
             results['vacc'].append(vacc)
-            results['vacc_o1'].append(vacc_o1)
-            results['vacc_o2'].append(vacc_o2)
             results['vprobs'].append(vprobs)
             results['ztz0_cos'].append(ztz0_cos)
             results['ztz0_norm'].append(ztz0_norm)
