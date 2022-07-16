dcd702327d14da5be3776c3bb9305d1166189460
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-15 17:29:48 -0700

    inital

---
+++ bashes/PT_C10_Res50.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH -A NLP-CDT-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --job-name=PT
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --time=36:00:00
+#SBATCH --mem-per-cpu=10G
+#SBATCH --output=./logs/stage1.txt 
+#SBATCH --gres=gpu:1
+
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+
+. /etc/profile.d/modules.sh
+module purge
+module load rhel8/default-amp
+module load python/3.8 cuda/11.0 cudnn/8.0_cuda-11.1
+
+source /home/sg955/functorch-env/bin/activate
+
+cd /home/sg955/GitWS/P2_better_v0/
+
+srun python main_PT.py \
+--batch_size 128  \
+--dataset cifar10 --figsize 224 --loss_type ce \
+--run_name C10_res50_PT \
+--epochs 200
