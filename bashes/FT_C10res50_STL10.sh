26073e1e902ab74194a082ba774fd9d51d14baf1
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-17 09:34:29 -0700

    inital

--- bashes/FT_C10res50_STL10.sh
+++ bashes/FT_C10res50_STL10.sh
@@ -26,7 +26,7 @@ cd /home/sg955/GitWS/P2_better_v0/
 srun python main_FT.py \
 --batch_size 128  --dataset stl10 --figsize 224 --loss_type ce --model resnet50 \
 --lr 0.0005 --epochs 200 --scheduler_type multistep --warmup 10 \
---s1 40 --s2 80 --s3 140 \
+--s1 40 --s2 80 --s3 180 \
 --work_dir ./results/C10_res50_PT \
 --alice_name Alice_resnet50_PT.pth \
 --LP_dir LP_C1050_STL10_2en4wd \
