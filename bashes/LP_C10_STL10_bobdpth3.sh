df9ee0d9d82b619145f82f5bd2186f7029dbd52d
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-16 23:33:42 -0700

    inital

--- bashes/LP_C10_STL10_bobdpth3.sh
+++ bashes/LP_C10_STL10_bobdpth3.sh
@@ -25,7 +25,7 @@ cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_LP.py \
 --batch_size 128  --dataset stl10 --figsize 32 --loss_type ce --model resnet18 --Bob_depth 1 \
---lr 0.0005 --weight_decay 0 --optim_type sgd --scheduler_type cosine --lr_min 0.0005 \
+--lr 0.0005 --weight_decay 0 --optim_type sgd --scheduler_type cosine --min_lr 0.0005 \
 --work_dir ./results/C10_res18_PT \
 --alice_name Alice_resnet18_PT.pth \
 --run_name LP_STL10_5en4nowdnodecay
