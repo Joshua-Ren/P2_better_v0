d5ac09556da711b91e7af5f364448f9749b0c790
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-17 09:48:49 -0700

    inital

--- bashes/LP_IN1Kres50_domquick.sh
+++ bashes/LP_IN1Kres50_domquick.sh
@@ -25,7 +25,7 @@ cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_LP.py \
 --batch_size 128  --dataset domain_quick --figsize 224 --loss_type ce --model resnet50 --num_workers 4 \
---lr 0.002 --weight_decay 0.05 \
+--lr 0.002 --weight_decay 0 --min_lr 0.002 \
 --work_dir ./results/IN1K_res50_PT \
 --alice_name resnet50-classification.pth \
---run_name LP_IN1K50_domquicknew_2en3wd_f224
+--run_name LP_IN1K50_domquicknew_2en3nowdnodecay_f224
