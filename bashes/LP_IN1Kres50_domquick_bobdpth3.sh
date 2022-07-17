3c69e8ceefabae3c41eb3d79f62ae23cd5ba94fb
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-17 00:17:06 -0700

    inital

---
+++ bashes/LP_IN1Kres50_domquick_bobdpth3.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#SBATCH -A NLP-CDT-SL2-GPU
+#SBATCH -p ampere
+#SBATCH --job-name=LP
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=36:00:00
+#SBATCH --mem-per-cpu=20G
+#SBATCH --output=./logs/stage1.txt 
+#SBATCH --gres=gpu:1
+
+
+export NCCL_P2P_DISABLE=1
+export NCCL_IB_DISABLE=1
+
+. /etc/profile.d/modules.sh
+module purge
+module load rhel8/default-amp
+module load python/3.8 cuda/11.0 cudnn/8.0_cuda-11.1
+
+source /home/sg955/functorch-env/bin/activate
+
+cd /home/sg955/GitWS/P2_better_v0/
+
+srun python main_LP.py \
+--batch_size 128  --dataset domain_quick --figsize 224 --loss_type ce --model resnet50 --num_workers 4 --Bob_depth 3 \
+--lr 0.002 --weight_decay 0 --min_lr 0.002 \
+--work_dir ./results/IN1K_res50_PT \
+--alice_name resnet50-classification.pth \
+--run_name LP_IN1K50_domquicknew_2en3nowdnodecay_bobdpth3
