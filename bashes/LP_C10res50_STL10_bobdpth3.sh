3b0d6b0b9d3cab3be62c2d0bac1eece10bda957b
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-17 10:00:11 -0700

    inital

--- bashes/LP_C10res50_STL10_bobdpth3.sh
+++ bashes/LP_C10res50_STL10_bobdpth3.sh
@@ -24,8 +24,8 @@ source /home/sg955/functorch-env/bin/activate
 cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_LP.py \
---batch_size 128  --dataset stl10 --figsize 32 --loss_type ce --model resnet18 --Bob_depth 3 \
---lr 0.0005 --weight_decay 0 --optim_type sgd --scheduler_type cosine --min_lr 0.0005 \
---work_dir ./results/C10_res18_PT \
---alice_name Alice_resnet18_PT.pth \
---run_name LP_STL10_5en4nowdnodecay_bobdpth3
+--batch_size 128  --dataset stl10 --figsize 224 --loss_type ce --model resnet50 --Bob_depth 3 \
+--lr 0.002 --weight_decay 0 \
+--work_dir ./results/C10_res50_PT \
+--alice_name Alice_resnet50_PT.pth \
+--run_name LP_C1050_STL10_2en3nowd_bobdpth3
