89a3d6e01717d6d0e886b0c51f724e733b0db45e
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-23 20:09:31 -0700

    inital

--- bashes/PT_C100_Res50.sh
+++ bashes/PT_C100_Res50.sh
@@ -25,6 +25,6 @@ cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_PT.py \
 --batch_size 128  \
---dataset cifar100 --figsize 224 --loss_type ce --model resnet50 \
---run_name C100_res50_PT \
+--dataset domain_real --figsize 224 --loss_type ce --model resnet50 \
+--run_name Domreal_res50_PT \
 --epochs 200
