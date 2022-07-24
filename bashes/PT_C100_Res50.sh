59b0135a487fce80ac01f4233cedb6b987f2fb25
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-24 00:39:38 -0700

    inital

--- bashes/PT_C100_Res50.sh
+++ bashes/PT_C100_Res50.sh
@@ -24,7 +24,7 @@ source /home/sg955/functorch-env/bin/activate
 cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_PT.py \
---batch_size 128  --lr 0.01 --warmup 20 \
+--batch_size 128  --lr 0.001 --min_lr 0.0001 \
 --dataset cifar100 --figsize 224 --loss_type ce --model resnet50 \
 --run_name C100_res50_PT \
 --epochs 200
