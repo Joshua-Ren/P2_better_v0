895092c16eaee75068bec844163335420b72000b
Joshua-Ren <renyi.joshua@gmail.com>
2022-07-31 23:01:50 -0700

    inital

--- bashes/Fig_sweep_IN1K50_STL10.sh
+++ bashes/Fig_sweep_IN1K50_STL10.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A NLP-CDT-SL2-GPU
 #SBATCH -p ampere
-#SBATCH --job-name=FT
+#SBATCH --job-name=Sweep
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
--- bashes/Fig_sweep_simclr50_domquick.sh
+++ bashes/Fig_sweep_simclr50_domquick.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -A NLP-CDT-SL2-GPU
 #SBATCH -p ampere
-#SBATCH --job-name=FT
+#SBATCH --job-name=Sweep
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=6
@@ -24,8 +24,8 @@ source /home/sg955/functorch-env/bin/activate
 cd /home/sg955/GitWS/P2_better_v0/
 
 srun python main_LPFT_sweep.py \
---batch_size 128  --dataset stl10 --figsize 224 --loss_type ce --model resnet50 --num_workers 4 \
+--batch_size 128  --dataset domain_quick --figsize 224 --loss_type ce --model resnet50 --num_workers 4 \
 --base_lr 0.01 --FT_epochs 100 \
 --work_dir ./results/IN1K_simclr_PT \
 --alice_name resnet50-simclr.pth --proj_name betterv0_tab1LPFT2 \
---run_name Sweep__simclr50_stl10
+--run_name Sweep__simclr50_domquick
